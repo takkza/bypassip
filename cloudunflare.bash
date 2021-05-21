745ada560d13dcb9c95876157c15710e40b53b13
SHADOW_TAK <84277891+takkza@users.noreply.github.com>
2021-05-21 17:51:04 +0700

    Update cloudunflare.bash

--- cloudunflare.bash
+++ cloudunflare.bash
@@ -1,3 +1,6 @@
+#!/bin/bash
+# CloudUnflare
+
 CompleteDNS_Login='email@mail.com|password'
 
 if [[ -z $(command -v dig) ]]; then
@@ -11,7 +14,7 @@ elif [[ -z $(command -v whois) ]]; then
 	exit
 fi
 
-echo 'bypass ip CLOUDFLARE'
+echo "BYPASS CLOUDFLARE"
 
 if [[ -f cuf-domain.tmp ]]; then
 	rm cuf-domain.tmp
@@ -34,7 +37,7 @@ fi
 
 function Dig() {
 	D=$1
-	echo " INFO: Checking ${D}"
+	echo " Checking ${D}"
 	for DMN in $(dig +short ${D} | grep '[.]'$ | sed 's/[.]$//g' | sort -V | uniq)
 	do
 		echo "   + CNAME: ${DMN}"
@@ -60,7 +63,7 @@ do
 	else
 		((c++))
 		if [[ $(expr $c % 20) -eq 0 ]]; then
-			echo " INFO: Subdomain enumeration progress [${i}/${max}]"
+			echo "Subdomain [${i}/${max}]"
 		fi
 	fi
 done
@@ -72,14 +75,13 @@ function CompleteDNS() {
 	PASS=$(echo ${CRE} | awk -F '|' '{print $2}')
 	TOKEN=$(curl -s --cookie-jar cookie.txt https://completedns.com/login | grep '_csrf_token' | sed 's/value="/\nToken /g' | grep ^Token | sed 's/"//g' | awk '{print $2}')
 	if [[ ! -z $(curl -skL --cookie cookie.txt --cookie-jar cookie.txt 'https://completedns.com/login_check' --data "_csrf_token=${TOKEN}&_username=${EMAIL}&_password=${PASS}&submitButton=" | grep 'Invalid credentials.') ]]; then
-		echo " ERROR: CompleteDNS cannot login"
+		echo "CompleteDNS cannot login"
 		return 1
 	fi
 	if [[ -f completedns.tmp ]]; then
 		rm completedns.tmp
 	fi
 	curl -s --cookie cookie.txt https://completedns.com/dns-history/ajax/?domain=${DMN} &>> completedns.tmp
-	echo " INFO: NS History by CompleteDNS.com"
 	i=0
 	IFS=$'\n'
 	for NSROW in $(cat completedns.tmp | sed ':a;N;$!ba;s/\n/ /g' | sed 's/clearfix/\n/g' | sed 's/col-md-2/\nASULAH/g' | grep ASULAH | sed 's/  //g' | sed 's/>/ /g' | sed 's/</ /g');
@@ -108,12 +110,13 @@ function ViewDNS() {
 	curl -s "https://viewdns.info/iphistory/?domain=${DMN}" -H 'user-agent: Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.120 Mobile Safari/537.36' --compressed &>> viewdns.tmp
 	COUNT=$(cat viewdns.tmp | sed ':a;N;$!ba;s/\n/ /g' | sed 's/<table border="1">/\nIPHISTORY/g' | sed 's/<\/table>/\n/g' | grep ^IPHISTORY | sed 's/<tr><td>/\n/g' | sed 's/\r//' | grep ^[0-9] | sed 's/<\/td><td>/|/g' | sed 's/<\/td><td align="center">/|/g' | sed 's/<\/td><\/tr>//g' | awk -F '|' '{print "   + "$4" | "$1" | "$3"("$2")"}' | sort -V | wc -l);
 	if [[ ${COUNT} -lt 1 ]]; then
-		echo " ERROR: No IP History data"
+		echo "No IP History"
 	else
-		echo " INFO: IP History"
+		echo "IP is"
 		cat viewdns.tmp | sed ':a;N;$!ba;s/\n/ /g' | sed 's/<table border="1">/\nIPHISTORY/g' | sed 's/<\/table>/\n/g' | grep ^IPHISTORY | sed 's/<tr><td>/\n/g' | sed 's/\r//' | grep ^[0-9] | sed 's/<\/td><td>/|/g' | sed 's/<\/td><td align="center">/|/g' | sed 's/<\/td><\/tr>//g' | awk -F '|' '{print "   + "$4" | "$1" | "$3"("$2")"}' | sort -V
 	fi
 	rm viewdns.tmp
 }
 
 ViewDNS ${DOMAIN}
+
